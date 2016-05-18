9305f8b4f6ae899494c65d9932d0c8a21383e092
Tsong Lee <l9tsong@gmail.com>
2016-05-18 11:03:03 +0800

    disable meta

--- library/structure/content-extensions.php
+++ library/structure/content-extensions.php
@@ -146,24 +146,24 @@ if ( ! function_exists( 'travelify_theloop_for_products' ) ) :
 <!--						</div>-->
                         <?php //do_action( 'travelify_after_post_content' ); ?>
 
-						<?php do_action( 'travelify_before_post_meta' ); ?>
+						<?php //do_action( 'travelify_before_post_meta' ); ?>
 
-						<div class="entry-meta-bar clearfix">
-							<div class="entry-meta">
-								<?php travelify_posted_on(); ?>
-								<?php if( has_category() ) { ?>
-									<span class="category"><?php the_category(', '); ?></span>
-								<?php } ?>
-								<?php if ( comments_open() ) { ?>
-									<span class="comments"><?php comments_popup_link( __( 'No Comments', 'travelify' ), __( '1 Comment', 'travelify' ), __( '% Comments', 'travelify' ), '', __( 'Comments Off', 'travelify' ) ); ?></span>
-								<?php } ?>
-							</div><!-- .entry-meta -->
-							<?php
-							echo '<a class="readmore" href="' . get_permalink() . '" title="'.the_title( '', '', false ).'">'.__( 'Read more', 'travelify' ).'</a>';
-							?>
-						</div>
+<!--						<div class="entry-meta-bar clearfix">-->
+<!--							<div class="entry-meta">-->
+<!--								--><?php //travelify_posted_on(); ?>
+<!--								--><?php //if( has_category() ) { ?>
+<!--									<span class="category">--><?php //the_category(', '); ?><!--</span>-->
+<!--								--><?php //} ?>
+<!--								--><?php //if ( comments_open() ) { ?>
+<!--									<span class="comments">--><?php //comments_popup_link( __( 'No Comments', 'travelify' ), __( '1 Comment', 'travelify' ), __( '% Comments', 'travelify' ), '', __( 'Comments Off', 'travelify' ) ); ?><!--</span>-->
+<!--								--><?php //} ?>
+<!--							</div><!-- .entry-meta -->-->
+<!--							--><?php
+//							echo '<a class="readmore" href="' . get_permalink() . '" title="'.the_title( '', '', false ).'">'.__( 'Read more', 'travelify' ).'</a>';
+//							?>
+<!--						</div>-->
 
-						<?php do_action( 'travelify_after_post_meta' ); ?>
+						<?php //do_action( 'travelify_after_post_meta' ); ?>
 
 					</article>
 				</section>
