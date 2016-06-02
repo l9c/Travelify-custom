7d89e433074edc0936ba832b25bf6b628f1dc36c
Tsong Lee <l9tsong@gmail.com>
2016-06-02 11:31:30 +0800

    js null op

--- library/js/social_custom.js
+++ library/js/social_custom.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function(){
 
 	jQuery(".social-icons ul li.google-plus a").html('<i class="fa fa-weixin"></i>');
 	jQuery(".social-icons ul li.google-plus a").append('<img class="hover-qrcode-img" src="'+jQuery(".social-icons ul li.google-plus a").attr('href')+'"/>');
-	jQuery(".social-icons ul li.google-plus a").attr('href','');
+	jQuery(".social-icons ul li.google-plus a").attr('href','javascript:;');
 
 	jQuery(".social-icons ul li.pinterest a").html('<i class="fa fa-weibo"></i>');
 	jQuery(".social-icons ul li.tumblr a").html('<i class="fa fa-qq"></i>');
